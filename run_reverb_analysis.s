f036a15db27564f4e5547cd0f6a5e9892c18d8ab
Helena Cuesta <helenacuesta@TestingAppInventor.local>
2019-10-12 12:24:44 +0200

    adding sbatch file

---
+++ run_reverb_analysis.s
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=reverb
+#SBATCH --nodes=1
+#SBATCH --time=10:00:00
+#SBATCH --mem=50
+#SBATCH --gres=gpu:1
+#SBATCH --output=reverb.out
+#SBATCH --error=reverb.err
+
+module purge
+
+module load cuda/10.1.105
+module load cudnn/7.0v4.0
+
+
+python predict_and_evaluate.py
