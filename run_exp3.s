3944a5eae69d9e7fcf6ce1fac3601bd4057c6c19
helenacuesta <hcuesta.8@gmail.com>
2020-04-27 15:12:27 +0200

    more exps

---
+++ run_exp3.s
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --job-name=exp3
+#SBATCH --nodes=1
+#SBATCH --time=144:00:00
+#SBATCH --mem=100GB
+#SBATCH --gres=gpu:1
+#SBATCH --output=exp3.out
+#SBATCH --error=exp3.err
+
+module purge
+
+module load cudnn/7.0v4.0
+module load cuda/10.1.105
+
+
+python 2_training.py --model model3 --save_key exp3multif0
+
