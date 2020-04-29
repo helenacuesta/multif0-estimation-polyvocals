e84ccb6923ba7dabca5567adf6d8073a88001947
helenacuesta <hcuesta@gmail.com>
2020-04-29 06:28:40 -0400

    counting diff

--- run_counting.s
+++ run_counting.s
@@ -2,13 +2,14 @@
 #
 #SBATCH --job-name=count
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
 #SBATCH --time=24:00:00
 #SBATCH --output=count.out
 #SBATCH --error=count.err
 
 
 module purge
-
+module load cudnn/7.0v4.0
+module load cuda/10.1.105
 
 python count_files.py
