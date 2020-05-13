ef5d661dbf907ea000ef35366ba73e07250bc7c7
helenacuesta <hcuesta.8@gmail.com>
2020-05-14 00:57:10 +0200

    fixing gpu bug

--- deepsalience/run_ds_testset.s
+++ deepsalience/run_ds_testset.s
@@ -2,14 +2,18 @@
 #
 #SBATCH --job-name=dstest
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=50:00:00
-#SBATCH --mem=16GB
+#SBATCH --time=70:00:00
+#SBATCH --mem=100GB
+#SBATCH --gres=gpu:1
 #SBATCH --output=dstest.out
 #SBATCH --error=dstest.err
 
 
 module purge
+
+
+module load cudnn/7.0v4.0
+module load cuda/10.1.105
 
 module load rubberband/intel/1.8.1
 module load ffmpeg/intel/3.2.2
