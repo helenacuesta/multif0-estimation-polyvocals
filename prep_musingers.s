0da996d952116cc9604c5ff616be6bbeb04130fa
helenacuesta <hcuesta@gmail.com>
2020-05-14 05:34:40 -0400

    editing

--- prep_musingers.s
+++ prep_musingers.s
@@ -1,15 +1,18 @@
 #!/bin/bash
 #
+
 #SBATCH --job-name=musingersprep
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=50:00:00
-#SBATCH --mem=16GB
+#SBATCH --time=48:00:00
+#SBATCH --mem=32GB
+#SBATCH --gres=gpu:1
 #SBATCH --output=musingersprep.out
 #SBATCH --error=musingersprep.err
 
-
 module purge
+
+module load cudnn/7.0v4.0
+module load cuda/10.1.105
 
 module load rubberband/intel/1.8.1
 module load ffmpeg/intel/3.2.2
